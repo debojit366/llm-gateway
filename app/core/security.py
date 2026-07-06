d8685d9d8fe0398dc9d00116f9c2243596ffaf31
debojit366 <debojitdas366@gmail.com>
2026-07-06 18:07:21 +0530

    feat: implement API key verification with rate limiting functionality

--- app/core/security.py
+++ app/core/security.py
@@ -2,14 +2,21 @@ from fastapi import Security, HTTPException, status
 from fastapi.security.api_key import APIKeyHeader
 from app.db.mongo import db_helper
 from typing import Dict, Any
+from datetime import datetime, timedelta
+
 
 API_KEY_HEADER = APIKeyHeader(name="X-API-KEY", auto_error=False)
+
+RATE_LIMIT_WINDOW = 60
+RATE_LIMIT_MAX_REQUESTS = 10
+
+
 
 async def verify_api_key(api_key_header: str = Security(API_KEY_HEADER)) -> Dict[str, Any]:
     if not api_key_header:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="X-API-KEY header missing hai bhai!"
+            detail="X-API-KEY header missing !"
         )
         
     db = getattr(db_helper, "db", None)
@@ -21,7 +28,29 @@ async def verify_api_key(api_key_header: str = Security(API_KEY_HEADER)) -> Dict
     if not user:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Invalid ya Deactivated API Key!"
+            detail="Invalid or Deactivated API Key!"
         )
+    user_id = user["_id"]
+    now = datetime.utcnow()
+    window_start = now - timedelta(seconds=RATE_LIMIT_WINDOW)
+
+    
+    request_count = await db.rate_limits.count_documents({
+        "user_id": user_id,
+        "timestamp": {"$gte": window_start}
+    })
+
+    if request_count >= RATE_LIMIT_MAX_REQUESTS:
+        raise HTTPException(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+            detail="Rate limit hit try again after 1 min."
+        )
+
+    await db.rate_limits.insert_one({
+        "user_id": user_id,
+        "timestamp": now
+    })
+    
+    await db.rate_limits.delete_many({"timestamp": {"$lt": window_start}})
         
     return user 
